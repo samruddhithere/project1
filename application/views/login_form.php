d2d2e1c0ff77326de3befcdd15e041811f6a3c41
Harshada <harshada.suryawanshi@clariontechnologies.co.in>
2019-02-07 15:12:40 +0530

    file updatedd

--- application/views/login_form.php
+++ application/views/login_form.php
@@ -9,7 +9,7 @@
     }
     ?>
     <head>
-        <title>Login Form123456</title>
+        <title>Login Form12345678910111213</title>
         <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/style.css">
         <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro|Open+Sans+Condensed:300|Raleway' rel='stylesheet' type='text/css'>
     </head>
